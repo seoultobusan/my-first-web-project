ee1f5fdca9c506b5bfb2193855e1a6eb30c5ff57
seoultobusan <notdol@DESKTOP-KROOQ33>
2023-04-27 21:27:14 +0900

    동적 렌더링 기능 추가

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
 import flask
+from flask import render_template, request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET',])
-def index():
-    return """
-<html>
-    <head>
-        <title>
-        this is my first web project
-        </title>
-    </head>
-    <body>
-        <div>
-            <h1>Hello World!</h1>
-            <p>
-                이것은 이우용의 첫번째 파이썬 정적 웹 프로젝트 입니다.
-            </p>
-            <a href="https://oboki.net">동일이 형 블로그로 이동하기</a>
-        </div>
-    <body>
-</html>
-    """
+@app.route('/<value>/', methods=['GET',])
+def index(value=''):
+    return render_template('my_template.html', value=value)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8888')
+    app.run(host='0.0.0.0', port='8080')
