4364b9111dcc3bd532a6ef691273f37e4c89c446
seoultobusan <notdol@DESKTOP-KROOQ33>
2023-04-27 21:09:11 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET',])
+def index():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port='8888')
